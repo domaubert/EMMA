60269eaac4d65f4e716f27501027c454bc8b4b67
Dominique Aubert <dominique.aubert@astro.unistra.fr>
2015-07-07 15:34:59 +0200

    ajout d'une fonction de cic pour les particules O2 par defaut dans le makefile

--- src/param.h
+++ src/param.h
@@ -4,26 +4,20 @@
 #define CFL (0.85)
 #define GRAV (0.25)
 #define FRACDX  (0.25)
-#define SEED (12345)
 #define PMIN 1e-12
-#define NCOSMOTAB (262144)
+#define NCOSMOTAB (128)
 #define VBC (30.); // relative DM velocity ala Tseliakovich & Hirata
 #define FRAC_VAR (0.1)
+#define SEED (4569) // srand seed for stochastic stars creation
 
+#define NGRP_SPACE (1)
+#define NGRP_TIME (1)
+#define NGRP (NGRP_SPACE * NGRP_TIME)
 
-#ifndef WHYDRO2
-  #define OMEGAB (0.0)
-#else
-  #define OMEGAB (0.049); // 0.049 for PLANCK
-//#define OMEGAB (0.31749); // 0.049 for PLANCK
-#endif
+#define NVAR_R (5)
+#define EMIN (1e-8)
+#define YHE (0)
 
-#ifdef WRAD
-  #define NGRP (1)
-  #define NVAR_R (5)
-  #define EMIN (1e-8)
-  #define NFLUX_R (6*NGRP*NVAR_R)
-#endif
 
 
 // ================= PHYSICAL CONSTANTS ===================
@@ -33,22 +27,25 @@
 #define AVOGADRO (6.02214129e23) // mol-1
 #define MYR (3.1536e13) // s
 #define PROTON_MASS (1.67262158e-27) //kg
-#define MHE_OVER_MH (4.002)
-#define HELIUM_MASS (MHE_OVER_MH*PROTON_MASS) //kg
 #define NEWTON_G (6.67384e-11) // SI
-
-#ifdef HELIUM
-#define YHE (0.24) // Helium Mass fraction
-#else
-#define YHE (0.) // Helium Mass fraction
-#endif
-
-#define yHE (YHE/(1.-YHE)/4.002) // Helium number fraction
+#define HELIUM_MASSFRACTION (0.24)
 #define MOLECULAR_MU (1.0)
 #define SOLAR_MASS (1.989e30) //kg
+#define KPLANCK (6.62617e-34)	//J/s
+#define ELECTRONVOLT (1.602176565e-19) //J
 
 
 // ================= SUPERNOVAE ===========================
+
+// Proportion of ejecta following fig 109 of the Starburst99 model:
+// http://www.stsci.edu/science/starburst99/figs/mass_inst_e.html
+#define EJECTA_PROP (0.5260172663907063)
+
+// Total feedback energy following fig 115 of the Starburst99 model :
+// http://www.stsci.edu/science/starburst99/figs/energy_inst_e.html
+#define SN_EGY (9.73565592733335e11) // j/kg
+
+
 
 //#define SN_EGY (3.7e11) 		// 3.7e15 erg.g-1 -> 3.7e11 J.kg-1 ->  Kay 2002   // 4e48 erg.Mo-1 springel hernquist 2003 -> OK
 //#define N_SNII (1.)
