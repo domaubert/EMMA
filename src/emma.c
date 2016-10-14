9b22badeb2e2e9f02479d000c62ec9afa97fd9a1
nico <nicolas.deparis@astro.unistra.fr>
2016-10-14 16:19:30 +0200

    move first setOclist to take into account setupMPI

--- src/emma.c
+++ src/emma.c
@@ -672,9 +672,6 @@ int main(int argc, char *argv[])
     struct CELL root;
     root = build_initial_grid(grid, firstoct, lastoct, &cpu, &param);
 
-    for (level=1; level<=param.lmax; level++){
-      setOctList(firstoct[level-1],&cpu,&param,level);
-    }
 
  // ==================================== assigning CPU number to levelcoarse OCTS // filling the hash table // Setting up the MPI COMMS
 
@@ -696,6 +693,9 @@ int main(int argc, char *argv[])
   newloadb=0;
 
 
+  for (level=1; level<=param.lmax; level++){
+    setOctList(firstoct[level-1],&cpu,&param,level);
+  }
 
 #if 0
 #ifdef WMPI
